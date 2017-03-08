cde939140a8bd6c76bd8f24c3c4bbd53ab8c7096
Foreseers <fforeseer@gmail.com>
2017-03-08 17:20:18 +0400

    Update

--- app/src/main/java/com/foreseer/dynamicuilist/RateFragment.java
+++ app/src/main/java/com/foreseer/dynamicuilist/RateFragment.java
@@ -7,57 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RateFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RateFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RateFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView firstCurrencyTextView;
+    private TextView secondCurrencyTextView;
+
+    private double firstToSecondRate;
+    private double secondToFirstRate;
 
     private OnFragmentInteractionListener mListener;
 
     public RateFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RateFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RateFragment newInstance(String param1, String param2) {
-        RateFragment fragment = new RateFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -86,23 +52,36 @@ public class RateFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+
+    @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public void changeCurrency(String firstCurrency, String secondCurrency, double rate){
+        if (firstCurrencyTextView == null) {
+            firstCurrencyTextView = (TextView) getView().findViewById(R.id.textView_firstCurrency);
+            secondCurrencyTextView = (TextView) getView().findViewById(R.id.textView_secondCurrency);
+        }
+
+        firstCurrencyTextView.setText(firstCurrency);
+        secondCurrencyTextView.setText(secondCurrency);
+        firstToSecondRate = rate;
+        secondToFirstRate = (1 / rate);
+
+        String rateString = "1 = " + String.valueOf(rate);
+        TextView rateTextView = (TextView) getView().findViewById(R.id.textView_currencyAmount);
+        rateTextView.setText(rateString);
     }
 }
